5c1690a0481efc279ec698b3eae1a7154d91e80e
Techxie-bc <donnaogbu@gmail.com>
2025-08-01 14:05:50 +0100

    about text corrected

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -19,8 +19,9 @@ export default function AboutUs() {
             />
             {/* Section Hero */}
             <SectionHero
-                aboutText="The Ministry of Health in Imo State serves as a vital engine for healthcare transformation, especially as the state seeks to promote public health and medical inclusion. While there's still room for growth and reform, the ministry represents a key opportunity for healthcare access and sustainable development in Imo State.\nThe Ministry of Health in Imo State is a government agency responsible for promoting healthcare access and regulating medical programs within the state. It is a strategic part of the state's development plan, especially considering Imo's diverse population.\nThe ministry was established to oversee and coordinate all healthcare services, including primary healthcare, disease prevention, and medical infrastructure development.\nOver the years, successive governments in Imo State have recognized the importance of providing quality healthcare services to all citizens, which are essential for the region's development and prosperity."
-                imgSrc="/images/health1.png"
+               aboutText="The Ministry of Health in Imo State serves as a vital engine for healthcare transformation, especially as the state seeks to promote public health and medical inclusion. While there's still room for growth and reform, the ministry represents a key opportunity for healthcare access and sustainable development in Imo State. The Ministry of Health in Imo State is a government agency responsible for promoting healthcare access and regulating medical programs within the state. It is a strategic part of the state's development plan, especially considering Imo's diverse population. The ministry was established to oversee and coordinate all healthcare services, including primary healthcare, disease prevention, and medical infrastructure development. Over the years, successive governments in Imo State have recognized the importance of providing quality healthcare services to all citizens, which are essential for the region's development and prosperity."
+ 
+               imgSrc="/images/health1.png"
                                   altText="Our Story - Imo State Ministry of Health conference event"
             />
            <TeamPage/>
